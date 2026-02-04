ea55ca2ebac33582af1320ebaf9a9a900efa5560
shravigaikwad08 <shravigaikwad08@gmail.com>
2026-02-04 15:39:01 +0530

    Create Parametrized.cpp

---
+++ Parametrized.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class add
+{
+public:
+    add(int a, int b)      {
+        int c = a + b;
+        cout << "Sum = " << c;
+    }
+};
+
+int main()
+{
+    add ob(20, 30);    
+    return 0;
+}
