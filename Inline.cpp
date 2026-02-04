ed3dad4a2466e0b464d3ca6560801f5264151f12
shravigaikwad08 <shravigaikwad08@gmail.com>
2026-02-04 15:37:16 +0530

    Create Inline.cpp

---
+++ Inline.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+inline int sq(int a)
+{
+    return a * a;
+}
+
+inline int cube(int a)
+{
+    return a * a * a;
+}
+
+int main()
+{
+    cout << "Sq of no = " << sq(5) << endl;
+    cout << "Cube of no = " << cube(2) << endl;
+    return 0;
+}
